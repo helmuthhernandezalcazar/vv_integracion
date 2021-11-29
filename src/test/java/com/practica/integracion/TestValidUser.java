fbba61ca86e1fa5b00d39e5eefe7608238684161
Victor <victor.alcaraz.lopez@alumnos.upm>
2021-11-29 18:42:08 +0100

    RamaVictor iniciada

--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -1,13 +1,22 @@
 package com.practica.integracion;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-public class TestValidUser {
 
-	/**
-	 * RELLENAR POR EL ALUMNO
-	 */
+public class TestValidUser {
+	@DisplayName("Test startRemoteSystem con usuario valido")
+	@Test
+	public void testStartRemoteSystem() {
+		boolean t = true;
+		assertTrue(t);
+	}
 
 }
