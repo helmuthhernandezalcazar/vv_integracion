6509288880f121fe28261b27c151babf548f5c7b
victoralcarazlopez <victor.alcaraz.lopez@alumnos.upm.es>
2021-12-04 19:44:09 +0100

    Test startRemoteSystem

--- src/test/java/com/practica/integracion/TestInvalidUser.java
+++ src/test/java/com/practica/integracion/TestInvalidUser.java
@@ -1,11 +1,48 @@
 package com.practica.integracion;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InOrder;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.practica.integracion.DAO.AuthDAO;
+import com.practica.integracion.DAO.GenericDAO;
+import com.practica.integracion.DAO.User;
+import com.practica.integracion.manager.SystemManager;
+import com.practica.integracion.manager.SystemManagerException;
 
 @ExtendWith(MockitoExtension.class)
 public class TestInvalidUser {
-	/**
-	 * RELLENAR POR EL ALUMNO
-	 */
+	@Mock
+	private AuthDAO mockAuthDAO;
+	
+	@Mock
+	private GenericDAO mockGenericDAO;
+	
+	private SystemManager systemManager;
+	List<Object> roles = new ArrayList<Object>();
+	private User userValido = new User ("1234", "Juan", "Garcia", "direccion", roles);
+	
+	@DisplayName("Test startRemoteSystem")
+	@Test
+	public void testStartRemoteSystem() throws SystemManagerException, OperationNotSupportedException {
+
+
+		
+	}
 }
--- src/test/java/com/practica/integracion/TestValidUser.java
+++ src/test/java/com/practica/integracion/TestValidUser.java
@@ -1,4 +1,10 @@
 package com.practica.integracion;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+
+import com.practica.integracion.DAO.*;
+import com.practica.integracion.manager.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,14 +15,79 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import javax.naming.OperationNotSupportedException;
+
+import org.mockito.InOrder;
+import org.mockito.InjectMocks;
+
+
 @ExtendWith(MockitoExtension.class)
 
 public class TestValidUser {
-	@DisplayName("Test startRemoteSystem con usuario valido")
+	
+	@Mock
+	private AuthDAO mockAuthDAO;
+	
+	@Mock
+	private GenericDAO mockGenericDAO;
+	
+	private SystemManager systemManager;
+	List<Object> roles = new ArrayList<Object>();
+	private User userValido = new User ("1234", "Juan", "Garcia", "direccion", roles);
+	
+	
+	@BeforeEach
+	public void setUp() {
+		roles.add("Jefe");
+		userValido.setRoles(roles);
+		systemManager = new SystemManager (mockAuthDAO, mockGenericDAO);
+	}
+	
+
+
+	
+	@DisplayName("Test startRemoteSystem")
 	@Test
-	public void testStartRemoteSystem() {
-		boolean t = true;
-		assertTrue(t);
+	public void testStartRemoteSystem() throws SystemManagerException, OperationNotSupportedException {
+		
+
+		//Simulación del comportamiento de authDAO
+		when(mockAuthDAO.getAuthData(userValido.getId())).thenReturn(userValido);
+		
+		//Simulacion de comportamiento para GenericDAO
+		List data = new ArrayList<>();
+		data.add("dato");
+		when(mockGenericDAO.getSomeData(userValido, "where id=0000")).thenReturn(data);
+	
+		//Probamos el metodo de SystemManager simulanco que el usuario es valido
+		Collection<Object> res = systemManager.startRemoteSystem(userValido.getId(), "0000");
+		
+		//Se prueba que se ha producido 1 llamada a AuthDAO
+		verify(mockAuthDAO, times(1)).getAuthData(userValido.getId());
+		
+		// Se prueba que se ha producido 1 llamada a GenericDAO
+		verify(mockGenericDAO, times(1)).getSomeData(userValido, "where id=0000");
+		
+		//Se prueba que la llamada a Generic DAO es posterior a AuthDAO
+		//despues el de  acceso a datos del sistema (la validaciones del orden en cada prueba)
+		InOrder ordered = inOrder(mockAuthDAO, mockGenericDAO);
+		// vemos si se ejecutan las llamadas a los dao, y en el orden correcto
+		ordered.verify(mockAuthDAO).getAuthData(userValido.getId());
+		ordered.verify(mockGenericDAO).getSomeData(userValido, "where id=0000");
+		
+		//Probar que el método de SystemManager ha devuelto información correctamente
+		//System.out.println("Roles: " + userValido.getRoles());
+		//System.out.println(res.toString());
+		assertEquals(res.toString(), "[dato]");
 	}
 
 }
